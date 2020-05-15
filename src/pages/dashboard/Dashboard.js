8b72fd6245541e549ce1da9a85afee3113b4b95d
KALPESH GHAG <kalpeshghag@KALPESHs-MacBook-Pro.local>
2020-05-15 18:38:18 +0530

    Initial Commit

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -17,6 +17,27 @@ export default class Dashboard extends Component {
   }
   render() {
     const { posts } = this.props
+    let pClass = [
+      styles.fontde,
+      styles.greyfont,
+      styles.small_font
+    ]
+    pClass = pClass.join(' ')
+    let h3Class = [
+      styles.fontde,
+      styles.medium_font
+    ]
+    h3Class = h3Class.join(' ')
+    let voteClass = [
+      styles.small_font,
+      styles.votecount
+    ] 
+    voteClass = voteClass.join(' ')
+    let votetagClass = [
+      styles.small_font,
+      styles.voteclass
+    ] 
+    votetagClass = votetagClass.join(' ')
     return (
       <div className={styles.outbox}>
         <div className={styles.maincontent}>
@@ -24,42 +45,7 @@ export default class Dashboard extends Component {
           <div className={styles.header}>
               Today
           </div>
-          <div className={styles.box}>
-            <ul>
-            <li>
-              <a href="">
-              <div className={styles.item}>
-                  <div className={styles.itemicon}>
-                    <img src="https://ph-files.imgix.net/cd4a0d8a-5d22-4d07-80f9-14a39e3ac9f4?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2" width="120" height="100" />
-                  </div>
-                  <div className={styles.itemdesc}>
-                    <h3 className={styles.itemdescheader}>
-                    Pearlii
-                    </h3>
-                    <p>Free, fast dental check-ups using AI image processing</p>
-                    <div className={styles.meta}>
-                      <div className={styles.metaaction}>
-                        <span>
-                        Comments:&nbsp;139
-                      </span>
-                      </div>
-                      <div className={styles.metainfo}>
-                        Android
-                      </div>
-                  </div>
-                  </div>
-                  <div className ={styles.itemright}>
-                        <span className={styles.voteclass}>
-                              Votes:
-                        </span>
-                  </div>
-                  
-              </div>
-              </a>
-              </li>
-            </ul>
-            </div>
-          </div>
+         <Post />
         </div>
       </div>
     )
--- src/pages/dashboard/components/post.js
+++ src/pages/dashboard/components/post.js
@@ -4,23 +4,62 @@ import styles from '../stylesheets/post.module.sass'
 
 export default function Post({ title, color, price, image }) {
   return (
-    <Card
-      border="primary"
-      className={`${styles.card} p-2`}
-      style={{ height: '100%' }}
-    >
-      <Card.Img className={styles.image} variant="top" src={image} />
-      <Card.Body>
-        <Card.Title className={styles.title}>{title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          <div className={styles.subtitle}>
-            {price}
+    // <Card
+    //   border="primary"
+    //   className={`${styles.card} p-2`}
+    //   style={{ height: '100%' }}
+    // >
+    //   <Card.Img className={styles.image} variant="top" src={image} />
+    //   <Card.Body>
+    //     <Card.Title className={styles.title}>{title}</Card.Title>
+    //     <Card.Subtitle className="mb-2 text-muted">
+    //       <div className={styles.subtitle}>
+    //         {price}
+    //       </div>
+    //     </Card.Subtitle>
+    //     <Card.Text className={styles.color}>
+    //       {color}
+    //     </Card.Text>
+    //   </Card.Body>
+    // </Card>
+    <div className={styles.box}>
+    <ul>
+    <li>
+      <a href="">
+      <div className={styles.item}>
+          <div className={styles.itemicon}>
+            <img src="https://ph-files.imgix.net/cd4a0d8a-5d22-4d07-80f9-14a39e3ac9f4?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2" width="120" height="100" />
           </div>
-        </Card.Subtitle>
-        <Card.Text className={styles.color}>
-          {color}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+          <div className={styles.itemdesc}>
+            <h3 className={h3Class}>
+            Pearlii
+            </h3>
+            <p className={pClass}>Free, fast dental check-ups using AI image processing</p>
+            <div className={styles.meta}>
+              <div className={styles.metaaction}>
+                <span>
+                Comments:&nbsp;139
+              </span>
+              </div>
+              <div className={styles.metainfo}>
+                Android
+              </div>
+          </div>
+          </div>
+          <div className ={styles.itemright}>
+            <span className={votetagClass}>
+                      Votes:
+                </span>
+                <span className={voteClass}>
+                  369
+            </span>
+          </div>
+          
+      </div>
+      </a>
+      </li>
+    </ul>
+    </div>
+  </div>
   )
 }
